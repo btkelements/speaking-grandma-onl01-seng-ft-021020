202db1a48e9d90a9d778278fe592ede7e2813cb3
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-14 02:07:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
     if phrase == "I LOvE YOU GRANDMA!"
-      return "I LOVe YOU TOO PUMPKIN!"
-    elseif phrase = phrase.downcase
+      return "I LOVE YOU TOO PUMPKIN!"
+    elseif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
+    elseif phrase == phrase.downcase
       
