d7c0bcb3b62d2696de59f3c15827d16e034a534b
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-02-14 02:16:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,19 +1,9 @@
 def speak_to_grandma(phrase)
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
-
-speak_to_grandma("HELLO THERE")
-
-
-
-# swap "not since 1938 statement with "huh speak up"
